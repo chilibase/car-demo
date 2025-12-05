586a98fc3851c3120a63d80313ec55f284f2dd2f
michalrakus <michalrakus@gmail.com>
2025-12-05 23:38:08 +0100

    lib version 0.1.2 - auth local, password change form

--- frontend/src/AppRouterProvider.tsx
+++ frontend/src/AppRouterProvider.tsx
@@ -5,6 +5,9 @@ import {CarBrowse} from "./forms/CarBrowse.tsx";
 import {ClientBrowse} from "./forms/ClientBrowse.tsx";
 import {CarReservationBrowse} from "./forms/CarReservationBrowse.tsx";
 import {XUserBrowse} from "@chilibase/frontend/XUserBrowse";
+import {XChangePasswordForm} from "@chilibase/frontend/XChangePasswordForm";
+import {XUtils} from "@chilibase/frontend/XUtils";
+import {XEnvVar, XViteAuth} from "@chilibase/frontend/XEnvVars";
 
 export const AppRouterProvider = () => {
     const router = createBrowserRouter([
@@ -18,7 +21,8 @@ export const AppRouterProvider = () => {
                 {path: "/cars", Component: CarBrowse},
                 {path: "/clients", Component: ClientBrowse},
                 {path: "/car-reservations", Component: CarReservationBrowse},
-                {path: "/users", Component: XUserBrowse}
+                {path: "/users", Component: XUserBrowse},
+                ...(XUtils.getEnvVarValue(XEnvVar.VITE_AUTH) === XViteAuth.LOCAL ? [{path: "/change-password", Component: XChangePasswordForm}] : [])
             ]
         }
     ]);
